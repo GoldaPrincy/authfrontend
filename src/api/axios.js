dea72c03bd531aaa8d2a439fa0ed17ba9f55989b
Golda Princy <goldaprincy@gmail.com>
2025-10-17 14:59:18 +0530

    five

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://authbackend-5s9n.onrender.com',
-  withCredentials: true
+  withCredentials: true,
 });
 export default api;
